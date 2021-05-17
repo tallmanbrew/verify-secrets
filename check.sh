59084e2ab617bd0e7a642be9826439e4ca595189
Chris Tallman <tallman.brew@gmail.com>
2021-05-17 09:45:47 -0400

    Update Action and Script

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ gh auth login --with-token < token.txt
 needs_exit="false"
 
 github_secrets=$(gh secret list --repo $REPO)
-if [ -z ${OWNER} ];
+if [ ${OWNER} != ''];
 then
     github_secrets+=$(gh secret list -o $OWNER)
 fi
