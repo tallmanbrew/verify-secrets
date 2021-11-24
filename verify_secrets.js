d76e4fee93be8ccfdfeb26db4e6711db80751f94
firenza <chad.oftedahl@sleepnumber.com>
2021-11-24 16:49:36 -0600

    Fail on missing secrets

--- verify_secrets.js
+++ verify_secrets.js
@@ -2,38 +2,50 @@ const core = require('@actions/core');
 const fs = require('fs');
 
 let verify_secrets = async function (secrets) {
-   
-    const parsedSecrets = JSON.parse(secrets);
 
-    core.info('Secrets available\n------------------------')
+  const parsedSecrets = JSON.parse(secrets);
 
-    let secretNames = []
-    for(var attributeName in parsedSecrets){
-      core.info(`Secret ${attributeName}`)
-      secretNames.push(attributeName);
+  core.info('Secrets available\n------------------------')
+
+  let secretNames = new Set()
+  for (var attributeName in parsedSecrets) {
+    core.info(attributeName)
+    secretNames.add(attributeName);
+  }
+
+  let referencedSecretNames = new Set()
+
+  const workflowFiles = await fs.promises.readdir(".github/workflows");
+
+  for (const workflowFile of workflowFiles) {
+    workflowFileBuffer = await fs.promises.readFile(`.github/workflows/${workflowFile}`);
+    workflowFileContent = workflowFileBuffer.toString();
+
+    const secretRegex = /\{\{\s*secrets\.(.*?)\s*\}/g;
+    let matches = [...workflowFileContent.matchAll(secretRegex)];
+
+    for (const match of matches) {
+      referencedSecretNames.add(match[1]);
+    }
+  }
+
+  core.info('\nSecrets referenced in workflows\n------------------------')
+
+  for (const referencedSecretName of Array.from(referencedSecretNames).sort()) {
+    core.info(referencedSecretName);
+  }
+
+  let missingSecretNames = new Set([...referencedSecretNames].filter(x => !secretNames.has(x)));
+
+  if (missingSecretNames.size > 0) {
+    core.info('\n!!! MISSING SECRETS !!\n------------------------');
+
+    for (const missingSecretName of Array.from(missingSecretNames).sort()) {
+      core.info(missingSecretName);
     }
 
-    let referencedSecretNames = new Set()
-
-    const workflowFiles = await fs.promises.readdir( ".github/workflows" );
-
-    for(const workflowFile of workflowFiles){
-      workflowFileBuffer = await fs.promises.readFile(`.github/workflows/${workflowFile}`);
-      workflowFileContent = workflowFileBuffer.toString();
-
-      const secretRegex = /\{\{\s*secrets\.(.*?)\s*\}/g;
-      let matches = [...workflowFileContent.matchAll(secretRegex)];
-
-      for(const match of matches){
-        referencedSecretNames.add(match[1]);
-      }
-    }
-
-    core.info('\nSecrets referenced in workflows\n------------------------')
-    
-    for(const referencedSecretName of Array.from(referencedSecretNames).sort()){
-      core.info(referencedSecretName);
-    }
+    core.setFailed();
+  }
 };
 
 module.exports = verify_secrets;
