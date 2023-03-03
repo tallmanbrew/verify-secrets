2ab98da8818fb3241284f254770ac831ca104718
Chris Tallman <chris.tallman@beckshybrids.com>
2023-03-03 15:27:23 -0500

    Clear out whitespace in line

--- check.py
+++ check.py
@@ -16,7 +16,7 @@ for filename in os.listdir(DIRECTORY):
     if filename.endswith(".yaml") or filename.endswith(".yml"):
          with open(os.path.join(DIRECTORY, filename), 'r') as f:
              for line in f:
-                 if line.startswith("#"):
+                 if line.strip().startswith("#"):
                      continue
                  res = re.search(r"\{\{(.*?)\}", line)
                  if (res and res.group(1) and 'secrets.' in res.group(1) and 'GITHUB_TOKEN' not in res.group(1)):
